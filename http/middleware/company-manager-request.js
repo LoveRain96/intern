8c0a130c995a06709f2de779788d5cadd49e76cd
duc <ducdt@sphinx-software.com>
2018-05-02 09:09:53 +0700

    update 2/5

--- http/middleware/company-manager-request.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (request, response, next) {
-    request.app.get('companyManager.factory').makeFromRequest(request.body).then(companyManager => {
-        request.manager = companyManager;
-        request.manager.setId(request.params.id);
-        next();
-    });
-};
